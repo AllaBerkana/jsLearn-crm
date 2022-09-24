1d1787c0ef84c5a158ed9dcf57538738eee7d627
Alla Berkana <allaberkana@gmail.com>
2022-09-24 23:18:25 +0300

    lesson-2-02

--- script/script.js
+++ script/script.js
@@ -1,2 +1,7 @@
 'use strict';
 
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('.modal__checkbox');
+const modalInputCheckbox = document.querySelector('.modal__input_discount');
+
